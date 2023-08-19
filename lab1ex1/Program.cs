1731d76b173930fd685c6859199a7604949f4ca8
Andrei Corovei <andrei.corovei@qubiz.com>
2023-08-19 11:21:16 +0300

    Add project files.

---
+++ lab1ex1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab1ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ex1 
+            Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui
+            paralelipiped dreptunghic, va afisa volumul lui */
+
+            int length;
+            int width;
+            int height;
+          
+            Console.WriteLine("Enter length: ");
+            length = int.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("Introduceti width: ");
+            width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti height: ");
+            height = int.Parse(Console.ReadLine());
+
+            int volum = length * width * height;
+
+            Console.WriteLine("The Rectangular parallelepiped volum is: " + volum);
+        }
+    }
+}
